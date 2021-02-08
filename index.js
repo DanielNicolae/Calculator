b6cd0b3e7ea415117261844913325a11a7b4b6f6
Daniel Nicolae <DeadlyPoet@yandex.com>
2021-02-08 16:26:41 +0200

    Get the numbers for the operation

--- index.js
+++ index.js
@@ -23,9 +23,99 @@ function divide(a, b) {
 }
 
 function changeDisplay(result) {
-    document.getElementById("result").textContent(result);
+    document.querySelector(".result").textContent = result;
 }
 
-document.getElementsByClassName("clear")[0].addEventListener("click", () => {
-    changeDisplay("");
-});
+
+
+function displayOperation() {
+    let numbersOp = "";
+    document.querySelector(".clear").addEventListener("click", () => {
+        changeDisplay("");
+        numbersOp = "";
+    });
+    let digits = document.querySelectorAll(".number, .operation, .dot");
+    for(let i = 0; i < digits.length; i++) {
+        digits[i].addEventListener("click", () => {
+            numbersOp += digits[i].textContent;
+            changeDisplay(numbersOp);
+        });
+    }
+}
+displayOperation();
+
+const operations = document.querySelectorAll(".operation");
+const digits = document.querySelectorAll(".number, .dot");
+let number1 = "";
+let number2 = "";
+let getFirstNum = true;
+
+function getNumbers() {
+
+    if(getFirstNum) {
+        operations.forEach(op => {
+            op.addEventListener("click", () => {
+                getFirstNum = false;
+                console.log(`numb1=${Number(number1)}`);
+                return Number(number1);
+            });
+        });
+    }
+    else {
+        document.querySelector(".equal").addEventListener("click", () => {
+            console.log(`numb2=${Number(number2)}`);
+            return Number(number2);
+        });
+    }
+   
+}
+
+
+function getOperationSymbol() {
+    operations.forEach(op => {
+        op.addEventListener("click", () => {
+            console.log(op.textContent);
+            return op.textContent;
+        });
+    });
+}
+
+digits.forEach(key => {
+    key.addEventListener("click", () => {
+        if(getFirstNum){
+            number1 += key.textContent;
+        }
+        else {
+            number2 += key.textContent;
+        }
+        getNumbers();
+    })
+})
+
+function evaluateOperation() {
+    document.querySelector(".equal").addEventListener("click", () => {
+        let result = 0;
+        switch (getOperationSymbol()) {
+            case "+":
+                console.log("+")
+                if(number2){
+                    result += add(number1, number2);
+                    console.log(`result= ${result}`);
+                }
+                break;
+            case "-":
+                result = subtract(number1, number2);
+                break;
+            case "x":
+                result = multiply(number1, number2);
+                break;
+            case "&#247;":
+                result = divide(number1, number2);
+                break;
+        }
+        console.log(`${number1}  ${number2} = ${result}`);
+        return result;
+    });
+}
+evaluateOperation();
+
